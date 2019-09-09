a45f98661f97780c9c7ff90542cbc2139792af06
Matthew White <Matthew.White02@gmail.com>
2019-09-09 11:30:41 +0100

    Immutability

--- src/com.mttwht.main.kt
+++ src/com.mttwht.main.kt
@@ -1,3 +1,25 @@
 fun main(args: Array<String>) {
     println("Hello, World!")
+
+    Immutability()
+}
+
+class Question {
+    var Answer:String = ""
+    val Question:String = "What is the meaning of life?"
+}
+
+fun Immutability() {
+    // mutable
+    var q = Question()
+    q = Question()
+
+    // immutable
+    val r = Question()
+//    r = Question() // Not allowed!
+    r.Answer = "42"
+//    r.Question = "" // Not allowed!
+
+    println(q.Question)
+    println(r.Answer)
 }
